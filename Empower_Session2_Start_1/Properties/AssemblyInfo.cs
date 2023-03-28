427065b399945d9f751922839b47024a0aba3c4e
Floris Cockaerts <floris.cockaerts@skyline.be>
2023-03-28 11:19:56 +0200

    Add project files.

---
+++ Empower_Session2_Start_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_Session2_Start_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_Session2_Start_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3B94413A-7884-4009-BAD3-E2857F322975")]
+[assembly: AssemblyVersion("1.0.0.0")]
